e21e579dc980217c4a490205c57874563e52e852
Aulya Anantha <aulyaanantha2402@gmail.com>
2025-03-24 20:43:23 +0700

    Commit Punya Anantha

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\DaftarController;
-use App\Http\Controllers\ProfilController;
-use App\Http\Controllers\RiwayatPesananController;
-use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\DetailProdukController;
-use App\Http\Controllers\PengaturanController;
-use App\Http\Controllers\DetailPesananController;
 
-Route::get('/login', [LoginController::class, 'index']);
-Route::get('/homepage', [HomepageController::class, 'index']);
 Route::get('/daftar', [DaftarController::class, 'index']);
-Route::get('/profil', [ProfilController::class, 'index']);
-Route::get('/riwayat-pesanan', [RiwayatPesananController::class, 'index']);
-Route::get('/produk', [ProdukController::class, 'index']);
 Route::get('/detail-produk', [DetailProdukController::class, 'index']);
-Route::get('/pengaturan', [PengaturanController::class, 'index']);
-Route::get('/detail-pesanan', [DetailPesananController::class, 'index']);

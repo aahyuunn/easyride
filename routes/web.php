1eeb6d7fd971fbb054a99606b2503a720aa73cd3
Aulya Anantha <aulyaanantha2402@gmail.com>
2025-03-24 20:47:02 +0700

    Hapus File

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,4 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DaftarController;
-use App\Http\Controllers\DetailProdukController;
 
-Route::get('/daftar', [DaftarController::class, 'index']);
-Route::get('/detail-produk', [DetailProdukController::class, 'index']);

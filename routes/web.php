e3aee18f86c665fcdb3247fa7dfc801f1282524f
Aulya Anantha <aulyaanantha2402@gmail.com>
2025-03-24 21:08:14 +0700

    Commit Anisa

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\DaftarController;
 use App\Http\Controllers\DetailProdukController;
+use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/daftar', [DaftarController::class, 'index']);
 Route::get('/detail-produk', [DetailProdukController::class, 'index']);
+Route::get('/homepage', [HomepageController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index']);
